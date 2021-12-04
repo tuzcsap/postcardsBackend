d3a5173980c63bc6d87134781894cd7ccf1083ac
tuzcsap <mikhail.miazin@yahoo.com>
2021-12-04 17:19:14 +0300

    * initial efforts on API

--- main.py
+++ main.py
@@ -10,25 +10,26 @@ app = FastAPI()
 client = motor.motor_asyncio.AsyncIOMotorClient(
     f"mongodb+srv://admin:{os.environ['MONGOPASSWORD']}@cluster0.kbeng.mongodb.net/PostcardsDB?retryWrites=true&w=majority")
 db = client.PostcardsDB
+collection = db.PostcardsCollection
 
-class Postcard(BaseModel):
-    id: int
-    url_front: Optional[str]
-    url_back: Optional[str]
-    text: Optional[str]
-    sent_date: Optional[str]
-    received_date: Optional[str]
-    from_address: Optional[str]
-    to_address: Optional[str]
-    lat_from: Optional[str]
-    lon_from: Optional[str]
-    lat_to: Optional[str]
-    lon_to: Optional[str]
-    sender_name: Optional[str]
-    receiver_name: Optional[str]
-    period: Optional[str]
-    tags: Optional[List[str]]
 
+# class Postcard(BaseModel):
+#     id: int
+#     url_front: Optional[str]
+#     url_back: Optional[str]
+#     text: Optional[str]
+#     sent_date: Optional[str]
+#     received_date: Optional[str]
+#     from_address: Optional[str]
+#     to_address: Optional[str]
+#     lat_from: Optional[str]
+#     lon_from: Optional[str]
+#     lat_to: Optional[str]
+#     lon_to: Optional[str]
+#     sender_name: Optional[str]
+#     receiver_name: Optional[str]
+#     period: Optional[str]
+#     tags: Optional[List[str]]
 
 
 @app.get("/")
@@ -36,6 +37,12 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/postcards/")
+async def get_all_postcards():
+    postcards = await collection.find().to_list(100)
+    return {"postcard": f"{postcards[1]}"}
+
+
+@app.get("/postcards/{id}")
+async def get_postcard_by_id(id: int):
+    return {"postcard": f"Postcard {id}"}
