84acd968defd43e2ea69b2a64ca56a2cd1c47c2e
tuzcsap <mikhail.miazin@yahoo.com>
2021-12-09 20:35:08 +0300

    * CORS: allow * for local dev

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import motor.motor_asyncio
 from pydantic import BaseModel
 from typing import Optional, List
+from bson import ObjectId
 
 load_dotenv()
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 client = motor.motor_asyncio.AsyncIOMotorClient(
     f"mongodb+srv://admin:{os.environ['MONGOPASSWORD']}@cluster0.kbeng.mongodb.net/PostcardsDB?retryWrites=true&w=majority")
 db = client.PostcardsDB
 collection = db.PostcardsCollection
 
 
-# class Postcard(BaseModel):
-#     id: int
-#     url_front: Optional[str]
-#     url_back: Optional[str]
-#     text: Optional[str]
-#     sent_date: Optional[str]
-#     received_date: Optional[str]
-#     from_address: Optional[str]
-#     to_address: Optional[str]
-#     lat_from: Optional[str]
-#     lon_from: Optional[str]
-#     lat_to: Optional[str]
-#     lon_to: Optional[str]
-#     sender_name: Optional[str]
-#     receiver_name: Optional[str]
-#     period: Optional[str]
-#     tags: Optional[List[str]]
+# TODO define response model
+class Postcard(BaseModel):
+    _id: ObjectId
+    id: int
+    url_front: Optional[str]
+    url_back: Optional[str]
+    text_language: Optional[str]
+    date_written: Optional[str]
+    date_sent: Optional[str]
+    date_received: Optional[str]
+    address_from: Optional[str]
+    address_to: Optional[str]
+    country_from: Optional[str]
+    country_to: Optional[str]
+    settlement_from: Optional[str]
+    settlement_to: Optional[str]
+    name_from: Optional[str]
+    name_to: Optional[str]
+    lat_from: Optional[str]
+    lng_from: Optional[str]
+    lat_to: Optional[str]
+    lng_to: Optional[str]
+    time_period: Optional[str]
+    tags: Optional[List[str]]
 
 
 @app.get("/")
@@ -37,12 +60,15 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/postcards/")
+# TODO filter postcards with null coords
+# TODO error if no postcards were found
+@app.get("/postcards", response_model=List[Postcard])
 async def get_all_postcards():
     postcards = await collection.find().to_list(100)
     return {"postcard": f"{postcards[1]}"}
 
 
-@app.get("/postcards/{id}")
+@app.get("/postcards/{id}", response_model=Postcard)
 async def get_postcard_by_id(id: int):
-    return {"postcard": f"Postcard {id}"}
+    postcard = await collection.find_one({"id": id})
+    return postcard
